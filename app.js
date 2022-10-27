5ae999dbd9f971560777f67446b6da8329510b3e
juanjovani <juandegante34@outlook.com>
2022-10-27 11:18:52 -0500

    funciones de botones

---
+++ app.js
@@ -0,0 +1,83 @@
+const urlBase= 'https://rickandmortyapi.com/api/character/';
+
+const loadData=(urlBase, page=1)=>{
+    const url=`${urlBase}?page=${page}`;
+ fetch(url)
+.then(respuesta =>{
+    return respuesta.json();
+}).then(respJson =>{
+    console.log(respJson);
+    const info=respJson.info;
+    const personajes=respJson.results;
+    console.log(info);
+    //validacion
+    const btnPrev =document.querySelector('#prev');
+    const btnNext =document.querySelector('#next');
+    if(!info.prev){
+        btnPrev.classList.add('disabled');
+
+    }else {
+        btnPrev.classList.remove('disabled');
+        btnPrev.setAttribute('data-id', Number(page) -1);
+    }
+    if(!info.next){
+        btnNext.classList.add('disabled');
+
+    }else {
+        btnNext.classList.remove('disabled');
+        btnNext.setAttribute('data-id', Number(page) +1);
+    }
+    showCharacters(personajes);
+})
+}
+const showCharacters=(personajes)=> {
+    const ListaPersonajes = document.querySelector('#characters');
+    //limpiar-
+    while(ListaPersonajes.firstChild){
+        ListaPersonajes.removeChild(ListaPersonajes.firstChild);
+    }
+    personajes.forEach(personaje => {
+        const div =document.createElement('div');
+        div.classList.add('col');
+        div.classList.add('m-2');;
+        div.innerHTML = creaCard(personaje);
+       ListaPersonajes.appendChild(div);
+        
+    });
+}
+const creaCard=(personaje)=>{
+    const html=`
+    <div class="card bg-dark text-ligth border" style="width: 18rem;">
+  <img src="${personaje.image}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${personaje.name}</h5>
+    <p class="card-text">${personaje.status}</p>
+    <a href="#" class="btn btn-primary"
+      data-bs-toggle="modal" 
+    data-bs-target="#exampleModal"
+    data id="${personaje.id}">Ver mas</a>
+    
+  </div>
+</div>
+`;
+return html;
+}
+const navegacion=(e)=>{
+
+    
+    if(e.target.classList.contains('btn')){
+        const id=e.target.getAttribute('data-id');
+        loadData(urlBase, id);
+    }
+}
+
+
+const loadInfo=(e)=>{
+    e.preventDefault();
+    if(e.target.classList.contains('btn')){
+        console.log('Hi!')
+    }
+}
+document.querySelector('#botones').addEventListener('click', navegacion);
+document.querySelector('#characters').addEventListener('click', loadInfo);
+loadData(urlBase);
